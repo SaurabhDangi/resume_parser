3f5d3c50ccb7932b5329e2040e7c12a727564ca6
SaurabhDangi <sauravdangi23@gmail.com>
2026-08-22 10:28:42 +0530

    Allow Vercel frontend

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5501",
-        "http://localhost:5501"
+        "http://localhost:5501",
+        "https://resume-parser-eight-sigma.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
