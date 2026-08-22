eaba2d9575f1bf50d713e135f66fd5f9f0ccd437
SaurabhDangi <sauravdangi23@gmail.com>
2026-08-22 09:35:36 +0530

    Configure CORS

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
+    allow_origins=[
+        "http://127.0.0.1:5501",
+        "http://localhost:5501"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
